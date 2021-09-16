ea9b16c4937b261741f240c5a36d63c389b28cdb
YoshihitoAso <y-aso@boostry.co.jp>
2021-09-16 19:32:51 +0900

    fix: inconsistent validations

--- app/model/schema/ledger.py
+++ app/model/schema/ledger.py
@@ -113,18 +113,18 @@ class RetrieveLedgerDetailsDataHistoryResponse(BaseModel):
 class RetrieveLedgerDetailsHistoryResponse(BaseModel):
     """Retrieve Ledger Details History schema (Response)"""
     token_detail_type: str
-    headers: List[dict]
+    headers: Optional[List[dict]]
     data: List[RetrieveLedgerDetailsDataHistoryResponse]
-    footers: List[dict]
+    footers: Optional[List[dict]]
 
 
 class RetrieveLedgerHistoryResponse(BaseModel):
     """Retrieve Ledger History schema (Response)"""
     created: str
     token_name: str
-    headers: List[dict]
+    headers: Optional[List[dict]]
     details: List[RetrieveLedgerDetailsHistoryResponse]
-    footers: List[dict]
+    footers: Optional[List[dict]]
 
 
 class LedgerDetailsDataTemplateResponse(BaseModel):
--- tests/test_app_routers_ledger_{token_address}_history_{ledger_id}_GET.py
+++ tests/test_app_routers_ledger_{token_address}_history_{ledger_id}_GET.py
@@ -621,6 +621,199 @@ class TestAppRoutersLedgerTokenAddressHistoryLedgerIdGET:
             ],
         }
 
+    # <Normal_3>
+    # All optional items are None
+    def test_normal_3(self, client, db):
+        user_1 = config_eth_account("user1")
+        issuer_address = user_1["address"]
+        token_address = "0xABCdeF1234567890abcdEf123456789000000000"
+        account_address_1 = "0xABCdeF1234567890abCDeF123456789000000001"
+        account_address_2 = "0xaBcdEF1234567890aBCDEF123456789000000002"
+        personal_info_contract_address = "0xabcDEF1234567890AbcDEf123456789000000003"
+
+        # prepare data
+        _token = Token()
+        _token.type = TokenType.IBET_STRAIGHT_BOND
+        _token.tx_hash = ""
+        _token.issuer_address = issuer_address
+        _token.token_address = token_address
+        _token.abi = {}
+        db.add(_token)
+
+        _ledger_1 = Ledger()
+        _ledger_1.token_address = token_address
+        _ledger_1.token_type = TokenType.IBET_STRAIGHT_BOND
+        _ledger_1.ledger = {
+            "created": "2022/12/01",
+            "token_name": "テスト原簿",
+            "headers": None,
+            "details": [
+                {
+                    "token_detail_type": "権利_test_1",
+                    "headers": None,
+                    "data": [
+                        {
+                            "account_address": account_address_1,
+                            "name": "name_test_1",
+                            "address": "address_test_1",
+                            "amount": 10,
+                            "price": 20,
+                            "balance": 30,
+                            "acquisition_date": "2022/12/02"
+                        },
+                        {
+                            "account_address": account_address_2,
+                            "name": "name_test_2",
+                            "address": "address_test_2",
+                            "amount": 100,
+                            "price": 200,
+                            "balance": 300,
+                            "acquisition_date": "2022/12/03"
+                        }
+                    ],
+                    "footers": None,
+                },
+                {
+                    "token_detail_type": "権利_test_2",
+                    "headers": None,
+                    "data": [
+                        {
+                            "account_address": None,
+                            "name": "name_test_1",
+                            "address": "address_test_1",
+                            "amount": 10,
+                            "price": 20,
+                            "balance": 200,
+                            "acquisition_date": "2020/01/01",
+                        },
+                        {
+                            "account_address": None,
+                            "name": "name_test_2",
+                            "address": "address_test_2",
+                            "amount": 20,
+                            "price": 30,
+                            "balance": 600,
+                            "acquisition_date": "2020/01/02",
+                        }
+                    ],
+                    "footers": None,
+                },
+            ],
+            "footers": None,
+        }
+        _ledger_1.ledger_created = datetime.strptime("2022/01/01 15:20:30", '%Y/%m/%d %H:%M:%S')  # JST 2022/01/02
+        db.add(_ledger_1)
+
+        # Note: account_address_1 only
+        _idx_personal_info_1 = IDXPersonalInfo()
+        _idx_personal_info_1.account_address = account_address_1
+        _idx_personal_info_1.issuer_address = issuer_address
+        _idx_personal_info_1.personal_info = {
+            "name": "name_db_1",
+            "address": "address_db_1"
+        }
+        db.add(_idx_personal_info_1)
+
+        _details_1 = LedgerDetailsTemplate()
+        _details_1.token_address = token_address
+        _details_1.token_detail_type = "権利_test_1"
+        _details_1.headers = None
+        _details_1.footers = None
+        _details_1.data_type = LedgerDetailsDataType.IBET_FIN
+        _details_1.data_source = token_address
+        db.add(_details_1)
+
+        # Mock
+        token = IbetStraightBondContract()
+        token.personal_info_contract_address = personal_info_contract_address
+        token.issuer_address = issuer_address
+        token_get_mock = mock.patch("app.model.blockchain.IbetStraightBondContract.get", return_value=token)
+        personal_get_info_mock = mock.patch("app.model.blockchain.PersonalInfoContract.get_info")
+
+        # request target API
+        with token_get_mock as token_get_mock_patch, personal_get_info_mock as personal_get_info_mock_patch:
+            # Note: account_address_2 only
+            personal_get_info_mock_patch.side_effect = [{
+                "name": "name_contract_2",
+                "address": "address_contract_2",
+            }]
+            resp = client.get(
+                self.base_url.format(token_address=token_address, ledger_id=1),
+                params={
+                    "latest_flg": 1,
+                },
+                headers={
+                    "issuer-address": issuer_address,
+                }
+            )
+
+            # assertion
+            token_get_mock_patch.assert_any_call(token_address)
+            personal_get_info_mock_patch.assert_has_calls([
+                call(account_address_2, default_value="")
+            ])
+
+        # assertion
+        assert resp.status_code == 200
+        assert resp.json() == {
+            "created": "2022/12/01",
+            "token_name": "テスト原簿",
+            "headers": None,
+            "details": [
+                {
+                    "token_detail_type": "権利_test_1",
+                    "headers": None,
+                    "data": [
+                        {
+                            "account_address": account_address_1,
+                            "name": "name_db_1",
+                            "address": "address_db_1",
+                            "amount": 10,
+                            "price": 20,
+                            "balance": 30,
+                            "acquisition_date": "2022/12/02"
+                        },
+                        {
+                            "account_address": account_address_2,
+                            "name": "name_contract_2",
+                            "address": "address_contract_2",
+                            "amount": 100,
+                            "price": 200,
+                            "balance": 300,
+                            "acquisition_date": "2022/12/03"
+                        }
+                    ],
+                    "footers": None,
+                },
+                {
+                    "token_detail_type": "権利_test_2",
+                    "headers": None,
+                    "data": [
+                        {
+                            "account_address": None,
+                            "name": "name_test_1",
+                            "address": "address_test_1",
+                            "amount": 10,
+                            "price": 20,
+                            "balance": 200,
+                            "acquisition_date": "2020/01/01",
+                        },
+                        {
+                            "account_address": None,
+                            "name": "name_test_2",
+                            "address": "address_test_2",
+                            "amount": 20,
+                            "price": 30,
+                            "balance": 600,
+                            "acquisition_date": "2020/01/02",
+                        }
+                    ],
+                    "footers": None,
+                },
+            ],
+            "footers": None,
+        }
+
     ###########################################################################
     # Error Case
     ###########################################################################
