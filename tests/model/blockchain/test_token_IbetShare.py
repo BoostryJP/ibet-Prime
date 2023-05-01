e9fa63bfb565da6eb02f3d21ab1b130f8c53d2dd
Yosuke Otosu <y-otosu@boostry.co.jp>
2023-05-01 21:31:00 +0900

    fix: tests

--- tests/model/blockchain/test_token_IbetShare.py
+++ tests/model/blockchain/test_token_IbetShare.py
@@ -604,9 +604,7 @@ class TestUpdate:
         assert share_contract.memo == ""
 
         _token_attr_update = db.query(TokenAttrUpdate).first()
-        assert _token_attr_update.id == 1
-        assert _token_attr_update.token_address == contract_address
-        assert _token_attr_update.updated_datetime > pre_datetime
+        assert _token_attr_update is None
 
     # <Normal_2>
     # Update all items
