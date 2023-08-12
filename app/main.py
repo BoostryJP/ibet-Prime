0f3e69b3e2b0931f3fb83373074c6f5514f02871
YoshihitoAso <yoshihito.asoh@gmail.com>
2023-08-12 18:12:56 +0900

    v23.9 doc

--- app/main.py
+++ app/main.py
@@ -59,7 +59,7 @@ tags_metadata = [
 app = FastAPI(
     title="ibet Prime",
     description="Security token management system for ibet network",
-    version="23.6.0",
+    version="23.9.0",
     contact={"email": "dev@boostry.co.jp"},
     license_info={
         "name": "Apache 2.0",
