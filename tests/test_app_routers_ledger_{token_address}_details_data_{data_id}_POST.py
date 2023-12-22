3ab3df568fd276542c1a84ed5395174c07c812bf
YoshihitoAso <yoshihito.asoh@gmail.com>
2023-12-22 18:27:51 +0900

    Fix upper limit check when uploading external data

--- tests/test_app_routers_ledger_{token_address}_details_data_{data_id}_POST.py
+++ tests/test_app_routers_ledger_{token_address}_details_data_{data_id}_POST.py
@@ -16,6 +16,7 @@ limitations under the License.
 
 SPDX-License-Identifier: Apache-2.0
 """
+import sys
 from unittest import mock
 
 from sqlalchemy import select
@@ -116,6 +117,76 @@ class TestAppRoutersLedgerTokenAddressDetailsDataDataIdPOST:
 
         mock_func.assert_called_with(token_address, db)
 
+    # <Normal_2>
+    # Max value
+    @mock.patch("app.routers.ledger.create_ledger")
+    def test_normal_2(self, mock_func, client, db):
+        user = config_eth_account("user1")
+        issuer_address = user["address"]
+        token_address = "0xABCdeF1234567890abcdEf123456789000000000"
+        data_id = "data_id_1"
+
+        # prepare data
+        _token = Token()
+        _token.type = TokenType.IBET_STRAIGHT_BOND.value
+        _token.tx_hash = ""
+        _token.issuer_address = issuer_address
+        _token.token_address = token_address
+        _token.abi = {}
+        _token.version = TokenVersion.V_23_12
+        db.add(_token)
+
+        _details_1_data_1 = LedgerDetailsData()
+        _details_1_data_1.token_address = token_address
+        _details_1_data_1.data_id = data_id
+        _details_1_data_1.name = "name_test_0"
+        _details_1_data_1.address = "address_test_0"
+        _details_1_data_1.amount = 0
+        _details_1_data_1.price = 1
+        _details_1_data_1.balance = 2
+        _details_1_data_1.acquisition_date = "2000/12/31"
+        db.add(_details_1_data_1)
+
+        # request target API
+        req_param = [
+            {
+                "name": "name_test_1",
+                "address": "address_test_1",
+                "amount": 1_000_000_000_000,
+                "price": 1_000_000_000_000,
+                "balance": sys.maxsize,
+                "acquisition_date": "2020/01/01",
+            },
+        ]
+        resp = client.post(
+            self.base_url.format(token_address=token_address, data_id=data_id),
+            json=req_param,
+            headers={
+                "issuer-address": issuer_address,
+            },
+        )
+
+        # assertion
+        assert resp.status_code == 200
+        assert resp.json() is None
+
+        _details_data_list = db.scalars(
+            select(LedgerDetailsData).order_by(LedgerDetailsData.id)
+        ).all()
+        assert len(_details_data_list) == 1
+
+        _details_data = _details_data_list[0]
+        assert _details_data.id == 2
+        assert _details_data.data_id == data_id
+        assert _details_data.name == "name_test_1"
+        assert _details_data.address == "address_test_1"
+        assert _details_data.amount == 1_000_000_000_000
+        assert _details_data.price == 1_000_000_000_000
+        assert _details_data.balance == sys.maxsize
+        assert _details_data.acquisition_date == "2020/01/01"
+
+        mock_func.assert_called_with(token_address, db)
+
     ###########################################################################
     # Error Case
     ###########################################################################
@@ -213,9 +284,9 @@ class TestAppRoutersLedgerTokenAddressDetailsDataDataIdPOST:
                 "1234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890"
                 "1",
                 "address": "address_test_1",
-                "amount": 1_000_000_000_001,
+                "amount": -1,
                 "price": -1,
-                "balance": 1_000_000_000_001 * 5_000_000_001,
+                "balance": -1,
                 "acquisition_date": "2020/01/01a",
             },
             {
@@ -223,9 +294,9 @@ class TestAppRoutersLedgerTokenAddressDetailsDataDataIdPOST:
                 "address": "1234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890"
                 "1234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890"
                 "1",
-                "amount": -1,
-                "price": 5_000_000_001,
-                "balance": -1,
+                "amount": 1_000_000_000_001,
+                "price": 1_000_000_000_001,
+                "balance": sys.maxsize + 1,
                 "acquisition_date": "2020/02/31",
             },
         ]
@@ -243,75 +314,74 @@ class TestAppRoutersLedgerTokenAddressDetailsDataDataIdPOST:
             "meta": {"code": 1, "title": "RequestValidationError"},
             "detail": [
                 {
-                    "ctx": {"max_length": 200},
-                    "input": "123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901",
+                    "type": "string_too_long",
                     "loc": ["body", 0, "name"],
                     "msg": "String should have at most 200 characters",
-                    "type": "string_too_long",
+                    "input": "123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901",
+                    "ctx": {"max_length": 200},
                 },
                 {
-                    "ctx": {"le": 1000000000000},
-                    "input": 1000000000001,
+                    "type": "greater_than_equal",
                     "loc": ["body", 0, "amount"],
-                    "msg": "Input should be less than or equal to 1000000000000",
-                    "type": "less_than_equal",
+                    "msg": "Input should be greater than or equal to 0",
+                    "input": -1,
+                    "ctx": {"ge": 0},
                 },
                 {
-                    "ctx": {"ge": 0},
-                    "input": -1,
+                    "type": "greater_than_equal",
                     "loc": ["body", 0, "price"],
                     "msg": "Input should be greater than or equal to 0",
+                    "input": -1,
+                    "ctx": {"ge": 0},
+                },
+                {
                     "type": "greater_than_equal",
-                },
-                {
-                    "ctx": {"le": 5000000000000000000000},
-                    "input": 5000000001005000000001,
                     "loc": ["body", 0, "balance"],
-                    "msg": "Input should be less than or equal to "
-                    "5000000000000000000000",
-                    "type": "less_than_equal",
+                    "msg": "Input should be greater than or equal to 0",
+                    "input": -1,
+                    "ctx": {"ge": 0},
                 },
                 {
-                    "ctx": {"max_length": 10},
-                    "input": "2020/01/01a",
+                    "type": "string_too_long",
                     "loc": ["body", 0, "acquisition_date"],
                     "msg": "String should have at most 10 characters",
-                    "type": "string_too_long",
+                    "input": "2020/01/01a",
+                    "ctx": {"max_length": 10},
                 },
                 {
-                    "ctx": {"max_length": 200},
-                    "input": "123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901",
+                    "type": "string_too_long",
                     "loc": ["body", 1, "address"],
                     "msg": "String should have at most 200 characters",
-                    "type": "string_too_long",
+                    "input": "123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901",
+                    "ctx": {"max_length": 200},
                 },
                 {
-                    "ctx": {"ge": 0},
-                    "input": -1,
-                    "loc": ["body", 1, "amount"],
-                    "msg": "Input should be greater than or equal to 0",
-                    "type": "greater_than_equal",
-                },
-                {
-                    "ctx": {"le": 5000000000},
-                    "input": 5000000001,
-                    "loc": ["body", 1, "price"],
-                    "msg": "Input should be less than or equal to 5000000000",
                     "type": "less_than_equal",
+                    "loc": ["body", 1, "amount"],
+                    "msg": "Input should be less than or equal to 1000000000000",
+                    "input": 1000000000001,
+                    "ctx": {"le": 1000000000000},
                 },
                 {
-                    "ctx": {"ge": 0},
-                    "input": -1,
+                    "type": "less_than_equal",
+                    "loc": ["body", 1, "price"],
+                    "msg": "Input should be less than or equal to 1000000000000",
+                    "input": 1000000000001,
+                    "ctx": {"le": 1000000000000},
+                },
+                {
+                    "type": "less_than_equal",
                     "loc": ["body", 1, "balance"],
-                    "msg": "Input should be greater than or equal to 0",
-                    "type": "greater_than_equal",
+                    "msg": "Input should be less than or equal to 9223372036854775807",
+                    "input": 9223372036854775808,
+                    "ctx": {"le": 9223372036854775807},
                 },
                 {
-                    "ctx": {"error": {}},
-                    "input": "2020/02/31",
+                    "type": "value_error",
                     "loc": ["body", 1, "acquisition_date"],
                     "msg": "Value error, The date format must be YYYY/MM/DD",
-                    "type": "value_error",
+                    "input": "2020/02/31",
+                    "ctx": {"error": {}},
                 },
             ],
         }
