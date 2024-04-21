2cb1f2dd068f0b5be013945507ea730d61ab701c
YoshihitoAso <yoshihito.asoh@gmail.com>
2024-04-21 23:47:39 +0900

    Bump hardhat

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,9 +4,8 @@ module.exports = {
     hardhat: {
       chainId: 2017,
       gasPrice: 0,
-      initialBaseFeePerGas: 0,
       blockGasLimit: 800000000,
-      hardfork: "merge",  // We should use "berlin", but because of the https://github.com/NomicFoundation/hardhat/issues/5052 issue we are using "merge".
+      hardfork: "berlin",
       throwOnTransactionFailures: false,
       throwOnCallFailures: false,
       allowBlocksWithSameTimestamp: true
