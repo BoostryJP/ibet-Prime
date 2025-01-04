5f78ba4370b1b496d565047ef33d2b0e0ce0da5c
YoshihitoAso <yoshihito.asoh@gmail.com>
2025-01-05 01:16:27 +0900

    fix

--- tests/app/test_freezelog_UpdateFreezeLog.py
+++ tests/app/test_freezelog_UpdateFreezeLog.py
@@ -443,7 +443,7 @@ class TestUpdateFreezeLog:
             )
 
         # Assertion
-        assert resp.status_code == 400
+        assert resp.status_code == 503
         assert resp.json() == {
             "meta": {"code": 2, "title": "SendTransactionError"},
             "detail": "failed to update log",
