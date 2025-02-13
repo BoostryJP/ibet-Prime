acf73728c8114d921eb0116c18405bf4f7087c54
Yosuke Otosu <y-otosu@boostry.co.jp>
2025-02-13 09:47:32 +0900

    fix tests

--- tests/app/test_ledger_RetrieveLedgerHistory.py
+++ tests/app/test_ledger_RetrieveLedgerHistory.py
@@ -1974,6 +1974,16 @@ class TestRetrieveLedgerHistory:
         _idx_personal_info_1.data_source = PersonalInfoDataSource.ON_CHAIN
         async_db.add(_idx_personal_info_1)
 
+        _idx_personal_info_2 = IDXPersonalInfo()  # Note: account_address_2 has personal information in DB
+        _idx_personal_info_2.account_address = account_address_2
+        _idx_personal_info_2.issuer_address = issuer_address
+        _idx_personal_info_2.personal_info = {
+            "name": "name_db_2",
+            "address": "address_db_2",
+        }
+        _idx_personal_info_2.data_source = PersonalInfoDataSource.ON_CHAIN
+        async_db.add(_idx_personal_info_2)
+
         _details_1 = LedgerDetailsTemplate()
         _details_1.token_address = token_address
         _details_1.token_detail_type = "権利_test_1"
@@ -1991,7 +2001,7 @@ class TestRetrieveLedgerHistory:
             },
             {"f-test1": "a", "f-test2": "b"},
         ]
-        _details_1.data_type = LedgerDataType.IBET_FIN.value
+        _details_1.data_type = LedgerDataType.IBET_FIN
         _details_1.data_source = token_address
         async_db.add(_details_1)
 
@@ -2015,12 +2025,12 @@ class TestRetrieveLedgerHistory:
             personal_get_info_mock as personal_get_info_mock_patch,
         ):
             # Note:
-            # account_address_2 has no personal information in the DB
+            # account_address_1 has no personal information in the DB
             # and gets information from the contract
             personal_get_info_mock_patch.side_effect = [
                 {
-                    "name": None,
-                    "address": None,
+                    "name": "name_contract_1",
+                    "address": "address_contract_1",
                 },
                 {
                     "name": "name_contract_2",
@@ -2071,8 +2081,8 @@ class TestRetrieveLedgerHistory:
                     "data": [
                         {
                             "account_address": account_address_1,
-                            "name": None,
-                            "address": None,
+                            "name": "name_contract_1",
+                            "address": "address_contract_1",
                             "amount": 10,
                             "price": 20,
                             "balance": 30,
@@ -2080,8 +2090,8 @@ class TestRetrieveLedgerHistory:
                         },
                         {
                             "account_address": account_address_2,
-                            "name": "name_contract_2",
-                            "address": "address_contract_2",
+                            "name": "name_db_2",
+                            "address": "address_db_2",
                             "amount": 100,
                             "price": 200,
                             "balance": 300,
@@ -2095,7 +2105,7 @@ class TestRetrieveLedgerHistory:
                         },
                         {"f-test1": "a", "f-test2": "b"},
                     ],
-                    "some_personal_info_not_registered": True,
+                    "some_personal_info_not_registered": False,
                 },
                 {
                     "token_detail_type": "権利_test_2",
@@ -2133,7 +2143,7 @@ class TestRetrieveLedgerHistory:
                         },
                         {"f-test1-1": "a", "f-test2-1": "b"},
                     ],
-                    "some_personal_info_not_registered": True,
+                    "some_personal_info_not_registered": False,
                 },
             ],
             "footers": [
