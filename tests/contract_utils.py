1a9ae76b4d295ee125aa171c8536ac2a39f7bc33
Yosuke Otosu <y-otosu@boostry.co.jp>
2025-03-27 19:22:34 +0900

    Update indexer_token_holders.py

--- tests/contract_utils.py
+++ tests/contract_utils.py
@@ -161,6 +161,17 @@ class IbetSecurityTokenContractTestUtils:
         ContractUtils.send_transaction(transaction=tx, private_key=private_key)
 
     @staticmethod
+    def force_unlock(contract_address: str, tx_from: str, private_key: str, args: list):
+        security_token_contract = ContractUtils.get_contract(
+            contract_name="IbetSecurityTokenInterface",
+            contract_address=contract_address,
+        )
+        tx = security_token_contract.functions.forceUnlock(*args).build_transaction(
+            {"chainId": CHAIN_ID, "from": tx_from, "gas": TX_GAS_LIMIT, "gasPrice": 0}
+        )
+        ContractUtils.send_transaction(transaction=tx, private_key=private_key)
+
+    @staticmethod
     def issue_from(contract_address: str, tx_from: str, private_key: str, args: list):
         security_token_contract = ContractUtils.get_contract(
             contract_name="IbetSecurityTokenInterface",
