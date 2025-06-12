50f50c178a30e62631ea13c06cbdbcc2eca2f5f1
yuki-iwasaki-boostry <y-iwasaki@boostry.co.jp>
2025-06-12 11:19:03 +0900

    Added batch details to the batch list

--- app/routers/issuer/bond.py
+++ app/routers/issuer/bond.py
@@ -19,6 +19,7 @@ SPDX-License-Identifier: Apache-2.0
 
 import json
 import uuid
+from collections import defaultdict
 from datetime import UTC, datetime
 from typing import Annotated, List, Optional, Sequence
 
@@ -1328,10 +1329,58 @@ async def list_all_batch_bond_redemption(
         stmt = stmt.offset(get_query.offset)
 
     _upload_list: Sequence[BatchIssueRedeemUpload] = (await db.scalars(stmt)).all()
+    upload_ids = [_upload.upload_id for _upload in _upload_list]
 
+    # Get Batch Records
+    record_list: Sequence[tuple[BatchIssueRedeem, IDXPersonalInfo | None]] = []
+    if upload_ids:
+        record_list = (
+            (
+                await db.execute(
+                    select(BatchIssueRedeem, IDXPersonalInfo)
+                    .outerjoin(
+                        IDXPersonalInfo,
+                        and_(
+                            BatchIssueRedeem.account_address
+                            == IDXPersonalInfo.account_address
+                        ),
+                    )
+                    .where(BatchIssueRedeem.upload_id.in_(upload_ids))
+                )
+            )
+            .tuples()
+            .all()
+        )
+
+    # Group records by upload_id
+    record_list_by_upload_id = defaultdict(list)
+    for record in record_list:
+        record_list_by_upload_id[record[0].upload_id].append(record)
+
+    personal_info_default = {
+        "key_manager": None,
+        "name": None,
+        "postal_code": None,
+        "address": None,
+        "email": None,
+        "birth": None,
+        "is_corporate": None,
+        "tax_category": None,
+    }
     uploads = []
     for _upload in _upload_list:
         created_utc = pytz.timezone("UTC").localize(_upload.created)
+        results = [
+            {
+                "account_address": record[0].account_address,
+                "amount": record[0].amount,
+                "status": record[0].status,
+                "personal_information": (
+                    record[1].personal_info if record[1] else personal_info_default
+                ),
+            }
+            for record in record_list_by_upload_id[_upload.upload_id]
+        ]
         uploads.append(
             {
                 "batch_id": _upload.upload_id,
@@ -1340,6 +1389,7 @@ async def list_all_batch_bond_redemption(
                 "token_address": _upload.token_address,
                 "processed": _upload.processed,
                 "created": created_utc.astimezone(local_tz).isoformat(),
+                "results": results,
             }
         )
 
