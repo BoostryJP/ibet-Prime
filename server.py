14db9161ba9cc91871c73b1356d144a9859a1caa
Yoshihito Aso <y-aso@boostry.co.jp>
2026-01-30 21:10:24 +0900

    Refactor server to use Gunicorn standard ASGI worker

--- server.py
+++ server.py
@@ -17,28 +17,14 @@ limitations under the License.
 SPDX-License-Identifier: Apache-2.0
 """
 
-import os
-
-from uvicorn.workers import UvicornWorker
+from gunicorn.workers.gasgi import ASGIWorker
 
 # NOTE:
 # Because the cache is deleted when all Worker processes is down,
 # create the shared memory in the main process and hold the value.
 __import__("app.utils.cache_utils")
 
-# uvicorn parameters
-WORKER_CONNECTIONS = (
-    int(os.environ.get("WORKER_CONNECTIONS"))
-    if os.environ.get("WORKER_CONNECTIONS")
-    else 100
-)
-
 
 # Worker class to load by gunicorn when server run
-class AppUvicornWorker(UvicornWorker):
-    CONFIG_KWARGS = {
-        "loop": "asyncio",
-        "http": "h11",
-        # NOTE: gunicorn don't support '--worker-connections' to uvicorn
-        "limit_concurrency": WORKER_CONNECTIONS,
-    }
+class AppWorker(ASGIWorker):
+    pass
